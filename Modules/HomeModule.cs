8480b2656903d8a78df9db3fec4e84fe47721bd6
Labrynth + Jackson <student@epicodus.net>
2016-02-18 08:44:50 -0800

    initial files

---
+++ Modules/HomeModule.cs
@@ -0,0 +1,14 @@
+using Nancy;
+using System.Collections.Generic;
+
+namespace ScrabbleNamespace
+{
+  public class HomeModule : NancyModule
+  {
+    public HomeModule()
+    {
+//       Get["/"] = _ =>
+//
+    }
+  }
+}
---
+++ Objects/Scrabble.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ScrabbleNamespace
+{
+  public class Scrabble
+  {
+
+    public Scrabble(string input)
+    {
+      
+    }
+
+  }
+}
