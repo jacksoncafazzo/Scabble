e7a9eca8d186bd6e583a3f039387f69ee4ae5ee8
Labrynth + Jackson <student@epicodus.net>
2016-02-18 09:33:53 -0800

    implement basic dictionary check and score tally

--- Objects/Scrabble.cs
+++ Objects/Scrabble.cs
@@ -6,10 +6,28 @@ namespace ScrabbleNamespace
 {
   public class Scrabble
   {
+    private string _word;
+    private Dictionary<char, int> _letterPoints = new Dictionary<char, int>{};
 
     public Scrabble(string input)
     {
-      
+      _word = input;
+    }
+
+    public int ScrabbleScore()
+    {
+      int score = 0;
+
+      _letterPoints.Add('k',5);
+      Console.WriteLine(_letterPoints['k']);
+      foreach (char letter in _word)
+      {
+        score += _letterPoints[letter];
+      }
+
+      // Console.WriteLine(_word[0]);
+      // Console.WriteLine(_word[1]);
+      return score;
     }
 
   }
